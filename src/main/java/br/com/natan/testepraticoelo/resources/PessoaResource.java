e663176b2dcae2b55d3b70e7ca4b9c87e51183ac
Natan Fagundes <natan.fags@gmail.com>
2019-05-21 19:43:47 -0300

    Testando classe de dominio - Pessoa

--- src/main/java/br/com/natan/testepraticoelo/resources/PessoaResource.java
+++ src/main/java/br/com/natan/testepraticoelo/resources/PessoaResource.java
@@ -1,16 +1,31 @@
 package br.com.natan.testepraticoelo.resources;
 
+import br.com.natan.testepraticoelo.domain.Pessoa;
+import br.com.natan.testepraticoelo.domain.enums.CodigoPessoa;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/pessoas")
 public class PessoaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "Rest esta funcionando";
+    public List<Pessoa> listar(){
+
+        Pessoa p1 = new Pessoa(1, CodigoPessoa.toEnum(1),"Maria","1111111-12");
+        Pessoa p2 = new Pessoa(2, CodigoPessoa.toEnum(2),"Jose","2222222-12");
+        Pessoa p3 = new Pessoa(3, CodigoPessoa.toEnum(1),"João","3333333-12");
+
+        List<Pessoa> listaPessoas = new ArrayList<>();
+        listaPessoas.add(p1);
+        listaPessoas.add(p2);
+        listaPessoas.add(p3);
+
+        return listaPessoas;
     }
 
 }
