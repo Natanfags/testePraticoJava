7f38b2dda61d3c799b5c5c4c8be088ef95599b39
Natan Fagundes <natan.fags@gmail.com>
2019-05-21 18:28:25 -0300

    Projeto criado

---
+++ src/main/java/br/com/natan/testepraticoelo/TestePraticoEloApplication.java
@@ -0,0 +1,13 @@
+package br.com.natan.testepraticoelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestePraticoEloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestePraticoEloApplication.class, args);
+	}
+
+}
