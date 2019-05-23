d401740200618b8028f098557cfab0b37e34360b
Natan Fagundes <natan.fags@gmail.com>
2019-05-23 08:27:01 -0300

    Validacao de CPF na insercao de Pessoa

--- src/main/java/br/com/natan/testepraticoelo/dto/PessoaNewDto.java
+++ src/main/java/br/com/natan/testepraticoelo/dto/PessoaNewDto.java
@@ -1,12 +1,22 @@
 package br.com.natan.testepraticoelo.dto;
 
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
 public class PessoaNewDto implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Integer codigo;
+
+    @NotEmpty(message = "Preenchimento obrigatório")
+    @Length(min = 5, max = 150, message = "O tamanho deve entre 5 e 80 caracteres")
     private String nome;
+
+    @NotEmpty(message = "Preenchimento obrigatório")
+    @CPF
     private String cpf;
 
     public PessoaNewDto(){
